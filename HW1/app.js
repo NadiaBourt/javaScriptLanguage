c7a575cbbc60ad431776ca13d27c445fbd1797ab
golan4ik51 <cheverdin.konst@gmail.com>
2022-10-29 19:49:34 +0300

    hw1 app

---
+++ HW1/app.js
@@ -0,0 +1,29 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+readline.question('Enter your number  ', number => {
+   console.log("number");
+    number = Math.round(number);
+   if (number<0)
+   {
+       number = -number;
+      sumDigits(number);
+   }
+   else {
+       sumDigits(number);
+   }
+});
+
+
+function sumDigits(number) {
+    let digit = 0;
+    let sum = 0;
+            while (number != 0) {
+                digit = number % 10;
+                sum = (sum + digit);
+                number = ((number - digit) / 10);
+            }
+            console.log("SUM Number", sum);
+            readline.close();
+    }
