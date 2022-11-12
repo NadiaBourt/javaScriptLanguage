6259f7a938618d45cfe6d6608ecf9eacf6db0ec8
golan4ik51 <cheverdin.konst@gmail.com>
2022-11-12 21:52:45 +0200

    hw15 added

--- hw15.js
+++ hw15.js
@@ -4,17 +4,34 @@ function minMax (array) {
     return array.reduce((prevValue, currentValue) => {
         prevValue[0] = prevValue[0] < currentValue ? prevValue[0] : currentValue;
         prevValue[1] = prevValue[1] > currentValue ? prevValue[1] : currentValue;
-        console.log()
+
         return prevValue;
 }, [array[0], array[0]]);
 }
-console.log(`ARRAY before: ${array}; OUTPUT array [min,max]: ${minMax(array)}`);
+console.log(`TASK 1. ARRAY before: ${array}; OUTPUT array [min,max]: ${minMax(array)}`);
 //2
 let arrayString = ["abc","old_abc","lmn","123","old_lmn","old_"];
 let prefix = "old_";
+let res;
 function deleteWithPrefix(arrayString, prefix) {
-   // return arrayString.filter(element=>!element.)}
-
-
-
+    res = arrayString.filter(item => item.indexOf(prefix));
+    return res;
 }
+console.log("TASK 2. ARRAY BEFORE ", arrayString, "ARRAY AFTER", deleteWithPrefix(arrayString, prefix));
+//3
+let inputArray = [1,6,3,8,5,2,7,4,9];
+let array2 = inputArray.slice();
+console.log(`TASK 3. Array before ${inputArray}`);
+
+function getSortedEvenOdd (array2) {
+    array2.sort((a, b) => a % 2 - b % 2 || a - b);
+
+
+    return array2;
+    }
+
+getSortedEvenOdd (array2);
+console.log("ARRAY AFTER", getSortedEvenOdd (array2));
+
+
+
