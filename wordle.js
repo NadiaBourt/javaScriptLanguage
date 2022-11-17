0a6e1a2dcc0583d14f5c38447509d783a27c5eb9
golan4ik51 <cheverdin.konst@gmail.com>
2022-11-17 20:57:06 +0200

    start

--- wordle.js
+++ wordle.js
@@ -1,13 +1,29 @@
 const word = "table";
 const N_LETTERS = 5;
+const letterElements = document.querySelectorAll(".letter-guess")
 function onChange (event)
 {
     const wordGuess = event.target.value;
+    event.target.value = '';
     if (wordGuess.length != N_LETTERS)
     {
         alert(`A word should contain ${N_LETTERS} letters`);
     }
 else {
-
+const wordAr = Array.from(wordGuess);//make array from enters word
+wordAr.forEach((l,i) => letterElements[i].innerHTML = l);
+        //l-буква i - index
+        // from 5 letters i need make 5 colors = it's MAP
+const colors = wordAr.map((letter,i) =>
+{
+  let index = word.indexOf(letter);
+  let res = 'red';//red = index =-1
+    if (index> -1){
+        res = index==i?'green' : 'yellow'         //index==i place and letter - green
     }
+    return  res;
+})
+        colors.forEach((c,i) => letterElements[i].style.color=c)
+}
+
 }
