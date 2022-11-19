3d12f5bd81957131a5613a138b81e54327fdf7fe
golan4ik51 <cheverdin.konst@gmail.com>
2022-11-19 20:03:31 +0200

    hw16 finish

--- wordle.js
+++ wordle.js
@@ -1,30 +1,51 @@
 const word = "table";
 const N_LETTERS = 5;
-let try = 6;
+let tryMax = 5;
 const letterElements = document.querySelectorAll(".letter-guess")
+const form = document.querySelector('form')
+let successCounter;
 function onChange (event)
 {
-    const wordGuess = event.target.value;
-    event.target.value = '';
-    if (wordGuess.length != N_LETTERS)
+    if (tryMax === 0)
     {
-        alert(`A word should contain ${N_LETTERS} letters`);
+        // alert(`GAME OVER`);
+        form.innerHTML = `GAME OVER`;
+        tryMax = 5;
     }
-else {
-const wordAr = Array.from(wordGuess);   //make array from enters word
-wordAr.forEach((l,i) => letterElements[i].innerHTML = l);
-        //l-буква i - index
-        // from 5 letters i need make 5 colors = it's MAP
-const colors = wordAr.map((letter,i) =>
-{
-  let index = word.indexOf(letter);
-  let res = 'red';//red = index =-1
-    if (index> -1)
+    else if (tryMax>0)
     {
-        res = index==i?'green' : 'yellow'         //index==i place and letter - green
-    }
-    return  res;
-})
-        colors.forEach((c,i) => letterElements[i].style.color=c)
+        successCounter = 0;
+        form.innerHTML = tryMax;
+        tryMax--;
+        const wordGuess = event.target.value;
+        event.target.value = '';
+        if (wordGuess.length != N_LETTERS) {
+            alert(`A word should contain ${N_LETTERS} letters`);
+        } else {
+            const wordAr = Array.from(wordGuess);   //make array from enters word
+            wordAr.forEach((l, i) => letterElements[i].innerHTML = l);
+            //l-буква i - index
+            // from 5 letters I need make 5 colors = it's MAP
+            const colors = wordAr.map((letter, i) => {
+                let index = word.indexOf(letter);
+                let res = 'red';//red = index =-1
+                if (index > -1) {                                        //index==i place and letter - green
+                    if (index == i)
+                    {
+                        res = 'green'
+                        successCounter++;
+                    }
+                    else {
+                        res = 'yellow'
+                    }
+                }
+
+                return res;
+            })
+            if (successCounter === N_LETTERS) {
+                form.innerHTML = `YOU ARE WIN !!!`;
+            }
+            colors.forEach((c, i) => letterElements[i].style.color = c)
+        }
     }
 }
