f424ae3abfe3d312916e91be534b59b77544df70
golan4ik51 <cheverdin.konst@gmail.com>
2022-11-21 13:32:44 +0200

    hw17(from YURI)

--- wordle.js
+++ wordle.js
@@ -1,51 +1,75 @@
-const word = "table";
+const words = ["pappy", "beach", "apple", "react", "basis",
+    "anger", "hello", "dress", "black"];
+let word;
 const N_LETTERS = 5;
-let tryMax = 5;
-const letterElements = document.querySelectorAll(".letter-guess")
-const form = document.querySelector('form')
-let successCounter;
-function onChange (event)
-{
-    if (tryMax === 0)
-    {
-        // alert(`GAME OVER`);
-        form.innerHTML = `GAME OVER`;
-        tryMax = 5;
-    }
-    else if (tryMax>0)
-    {
-        successCounter = 0;
-        form.innerHTML = tryMax;
-        tryMax--;
-        const wordGuess = event.target.value;
-        event.target.value = '';
-        if (wordGuess.length != N_LETTERS) {
-            alert(`A word should contain ${N_LETTERS} letters`);
-        } else {
-            const wordAr = Array.from(wordGuess);   //make array from enters word
-            wordAr.forEach((l, i) => letterElements[i].innerHTML = l);
-            //l-буква i - index
-            // from 5 letters I need make 5 colors = it's MAP
-            const colors = wordAr.map((letter, i) => {
-                let index = word.indexOf(letter);
-                let res = 'red';//red = index =-1
-                if (index > -1) {                                        //index==i place and letter - green
-                    if (index == i)
-                    {
-                        res = 'green'
-                        successCounter++;
-                    }
-                    else {
-                        res = 'yellow'
-                    }
-                }
+const letterElements = document.querySelectorAll(".letter-guess");
+const trialsElement = document.querySelector(".guess-trials");
+const gameOverElement = document.querySelector(".game-over-message");
+const playAgainElement = document.getElementById("play-again");
+let flGameOver = false;
+const INITIAL_TRIALS = 6;
+let trials = INITIAL_TRIALS;
+function showTrialsMessage(trials) {
 
-                return res;
-            })
-            if (successCounter === N_LETTERS) {
-                form.innerHTML = `YOU ARE WIN !!!`;
-            }
-            colors.forEach((c, i) => letterElements[i].style.color = c)
-        }
-    }
+    trialsElement.innerHTML =
+        `remained ${trials} guess trials`;
+
+
 }
+function startGame() {
+    let index = Math.floor(Math.random() * words.length);
+    word = words[index];
+    trials = INITIAL_TRIALS
+    showTrialsMessage(trials);
+    gameOverElement.innerHTML ='';
+    playAgainElement.style.display='none';
+    letterElements.forEach(e => e.innerHTML='');
+    flGameOver = false;
+}
+function onChange(event) {
+    const wordGuess = event.target.value;
+
+    if (flGameOver) {
+        alert("game is over");
+        return;
+    }
+    trials--;
+    showTrialsMessage(trials);
+
+    if (wordGuess.length != N_LETTERS) {
+        alert(`A word should contain ${N_LETTERS} letters`)
+    } else {
+        const wordAr = Array.from(wordGuess);
+        wordAr.forEach((l, i) => letterElements[i].innerHTML = l);
+        const colors = wordAr.map((letter, i) => {
+            let index = word.indexOf(letter);
+            let res = 'red';
+            if (index  > -1) {
+                res = letter == word[i] ? 'green' : 'yellow'
+            }
+            return res;
+        })
+        colors.forEach((c, i) =>
+            letterElements[i].style.color=c)
+    }
+    const res = wordGuess == word;
+    if (trials == 0 || res) {
+        endGame(res);
+    }
+
+}
+function endGame(isSuccess) {
+    if (isSuccess) {
+        gameOverElement.innerHTML =  "Congratulations you are winner";
+        gameOverElement.style.color = "green"
+    } else {
+        gameOverElement.innerHTML =  "Sorry you are loser";
+        gameOverElement.style.color = "red"
+    }
+
+    playAgainElement.style.display='block';
+    trialsElement.innerHTML = '';
+    flGameOver = true;
+}
+startGame()
+
