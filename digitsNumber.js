be2c52e249b2413b7ea8f786093d3b3c878910b6
golan4ik51 <cheverdin.konst@gmail.com>
2022-12-26 12:24:30 +0200

    JS50(hw11) finish

--- digitsNumber.js
+++ digitsNumber.js
@@ -1,27 +1,64 @@
-{
-    const number=5623;
+
+    const number = 5623;
     let sum = 0;
 
-    console.log (" Output 1 = ", getDigitsSum(number));
+    console.log(" Output 1 = ", getDigitsSum(number));
     let example = "9000/(10+20)**2";
-    console.log("Output 2 = ", oPa (example));
+    let ex2 = "9+100/2";
+    console.log("Output 2 = ", oPa(example));
+    console.log("Output 3 = ", oPa(ex2));
 
 
-    function oPa (example)
-    {
-        return new Function('return '+ example)();
+    function oPa(example) {
+        return new Function('return ' + example)();
     }
 
-function getDigitsSum (number) {
-      if (number < 0) {
-        number = -number;
-    }
-        while (number != 0) {
+    function getDigitsSum(number) {
+        if (number < 0) {
+            number = Math.trunc(Math.abs(number));
+        }
+        do {
             let digits = 0;
             digits = Math.round(number % 10);
             sum = (sum + digits);
             number = ((number - digits) / 10);
-        }
+        } while (number != 0)
         return sum;
-}
-}
+    }
+
+    function printAnanas() {
+        const resultAN = ("A" + + "A" + "AS").toLowerCase();
+        console.log(resultAN);
+    }
+    printAnanas();
+    let num1 = 123.45;
+    let num2 = -280.123;
+    let num3 = 123;
+    let num4 = -123;
+
+    console.log(" Output reverse 1 = ", calling (num1));
+    console.log(" Output reverse 2 = ", calling (num2));
+    console.log(" Output reverse 3 = ", calling (num3));
+    console.log(" Output reverse 4 = ", calling (num4));
+
+
+
+    function calling(num) {
+        let res = num < 0? "-" : "";
+            if (num<0)
+            {
+                num = Math.abs(num)
+            }
+            num = Math.trunc(num);
+        do {
+            let digit = num % 10;
+            res =  res + digit;
+            num = Math.trunc(num / 10);
+        }
+       while (num != 0)
+        return res;
+    }
+
+    // function myParseInt (num, base){
+    //
+    // }
